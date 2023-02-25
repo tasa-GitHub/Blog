02a4178e902df88382db053fd89f073c42dbb2cc
polskaa <sen.0128.111@gmail.com>
2023-02-25 11:48:29 +0900

    global.cssをscss化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Inter, Raleway } from 'next/font/google'
 import { useEffect } from 'react'
